cbdbd93fc1927958451fcb1abffc27c46502b3ec
Albert Miftar <a.miftar@aa.mk>
2020-06-22 13:52:47 +0200

    Updates

---
+++ precache-manifest.b3159e84c6323b04e1978e2bb94a9e9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb56905268175cde60127995a7925db2",
+    "url": "/%7Bburger%7D/index.html"
+  },
+  {
+    "revision": "8761d25ab806c5262b54",
+    "url": "/%7Bburger%7D/static/css/main.1cd09a0d.chunk.css"
+  },
+  {
+    "revision": "c19e014138590fe51b65",
+    "url": "/%7Bburger%7D/static/js/2.ccb408ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Bburger%7D/static/js/2.ccb408ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8761d25ab806c5262b54",
+    "url": "/%7Bburger%7D/static/js/main.192a0eb4.chunk.js"
+  },
+  {
+    "revision": "533926f04a640d44ca23",
+    "url": "/%7Bburger%7D/static/js/runtime-main.33edc19d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/%7Bburger%7D/static/media/logo.b8503d26.png"
+  }
+]);
